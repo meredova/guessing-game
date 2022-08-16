d9c437d93e0f55fd3e773f80ac497d28e62bf4be
meredova <olya.gurbo@gmail.com>
2022-08-16 16:46:29 +0300

    docs: add working code in index.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.mid = 0;
+    }
 
     setRange(min, max) {
-
+        if (min > max) {
+            return;
+        }
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mid = Math.round((this.max - this.min) / 2 + this.min);
+        return this.mid;
     }
 
     lower() {
+        this.max = this.mid;
 
     }
 
     greater() {
+        this.min = this.mid;
 
     }
 }
